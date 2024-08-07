c562ef9845bfff49c06438e390b9a46c28974496
Derzsi Dezső <derzsidezso@gmail.com>
2024-08-07 22:23:28 +0300

    rebuilt TheNavigationView added

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="w-full border-b-2 border-gray-300">
-    <div class="max-w-7xl mx-auto p-5">
-      <ul class="w-full flex justify-end gap-10">
-        <router-link :to="{ name: 'home' }">Home</router-link>
-        <router-link v-if="user" :to="{ name: 'createEvent' }">Create Event</router-link>
-        <router-link v-if="user" :to="{ name: 'myEvents' }">MyEvents</router-link>
-        <router-link v-if="user" :to="{ name: 'profile' }">MyProfile</router-link>
-        <router-link v-if="!user" :to="{ name: 'signup' }">SignUp</router-link>
-        <router-link v-if="!user" :to="{ name: 'login' }">LogIn</router-link>
-        <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
-      </ul>
-    </div>
+  <div class="flex justify-between items-center max-w-7xl mx-auto py-8 border-b-2 border-gray-300">
+    <router-link :to="{ name: 'home' }">
+      <h1 class="pl-5 font-bold text-2xl text-green-800 xl:pl-0">Eventify</h1>
+    </router-link>
+
+    <ul class="hidden w-full justify-end gap-10 sm:flex sm:pr-5 xl:pr-0">
+      <router-link :to="{ name: 'home' }" class="hover:text-green-800">Home</router-link>
+      <router-link v-if="user" :to="{ name: 'createEvent' }" class="hover:text-green-800">
+        Create Event
+      </router-link>
+      <router-link v-if="user" :to="{ name: 'myEvents' }" class="hover:text-green-800">
+        MyEvents
+      </router-link>
+      <router-link v-if="user" :to="{ name: 'profile' }" class="hover:text-green-800">
+        MyProfile
+      </router-link>
+      <router-link v-if="!user" :to="{ name: 'signup' }" class="hover:text-green-800">
+        SignUp
+      </router-link>
+      <router-link v-if="!user" :to="{ name: 'login' }" class="hover:text-green-800">
+        LogIn</router-link
+      >
+      <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
+    </ul>
   </div>
 </template>
 
